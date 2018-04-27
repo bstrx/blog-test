8eb94b079614027eee2432e482b7ee4fb70431a6
Vladimir Prudilin <bstrxx@gmail.com>
2018-04-27 19:07:47 +0400

    Add most used words and other improvements

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -3,11 +3,16 @@
 namespace App\Controller;
 
 use App\Entity\Post;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class PostController extends Controller
 {
+    const IMAGE_EXTENSIONS_WHITELIST = ['jpg', 'png'];
+    const USER_EMAIL = 'vladimir.prudilin@opensoftdev.ru';
+
     /**
      * @return Response
      */
@@ -28,23 +33,9 @@ class PostController extends Controller
             ]
         ];
 
-        //TODO read from file
-
-        $posts = [];
-        foreach ($postsInfo as $postInfo) {
-            $post = new Post();
-            $post->setTime($postInfo['time']);
-            $post->setTitle($postInfo['title']);
-            $post->setContent($postInfo['content']);
-
-            $posts[] = $post;
-        }
-
-        $mostUsedWords = ['First', 'Second', 'Third', 'Fourth', 'Five'];
-
         return new Response($this->render('index.html.twig', [
-            'posts' => $posts,
-            'mostUsedWords' => $mostUsedWords,
+            'posts' => $this->getPosts(),
+            'mostUsedWords' => $this->getMostUsedWords(),
             'addPostUrl' => $this->generateUrl('add-article')
         ]));
     }
@@ -56,20 +47,113 @@ class PostController extends Controller
     public function addPostAction(Request $request) : Response
     {
         //TODO add validation
-        //TODO save to file
+        if ($request->get('email') !== self::USER_EMAIL) {
+            return new Response('TODO BAD EMAIL');
+        }
 
         $post = new Post();
         $post->setTime($request->get('time'));
         $post->setTitle($request->get('title'));
         $post->setContent($request->get('content'));
 
-        file_put_contents($_SERVER['DOCUMENT_ROOT'] . "fileStorage/posts.txt", serialize($post));
+        /** @var UploadedFile $file */
+        $file = $request->files->get('image');
+        if ($file) {
+            $extension = $file->guessExtension();
+            if (in_array($extension, self::IMAGE_EXTENSIONS_WHITELIST)) {
+                $imageName = uniqid() . '.' . $extension;
+                $file->move($this->getImageDir(), $imageName);
+                $post->setImage($imageName);
+            }
+        }
 
-        $serializedPost = file_get_contents($_SERVER['DOCUMENT_ROOT'] . "fileStorage/posts.txt");
-        print_r(unserialize($serializedPost));
-
-
+        $posts = $this->getPosts();
+        $posts[] = $post;
+        $this->putPosts($posts);
+        $this->updateMostUsedWords($posts);
 
         return new Response($this->render('post.html.twig', ['post' => $post]));
+    }
+
+    /**
+     * @return Post[]
+     */
+    private function getPosts()
+    {
+        $serializedPosts = file_get_contents($this->getFilePath('posts.txt'));
+        if ($serializedPosts) {
+            $posts = array_reverse(unserialize($serializedPosts));
+        } else {
+            $posts = [];
+        }
+
+        return $posts;
+    }
+
+    /**
+     * @param Post[] $posts
+     * @return bool|int
+     */
+    private function putPosts(array $posts)
+    {
+        return file_put_contents($this->getFilePath('posts.txt'), serialize($posts));
+    }
+
+    /**
+     * @return string[]
+     */
+    private function getMostUsedWords()
+    {
+        return unserialize(file_get_contents($this->getFilePath('mostUsedWords.txt')));
+    }
+
+    /**
+     * @param Post[] $posts
+     * @return array
+     */
+    private function countMostUsedWords(array $posts)
+    {
+        $allContent = array_reduce($posts, function ($carry, Post $post) {
+            $carry .= ' ' . $post->getContent();
+
+            return $carry;
+        });
+
+        $words = array_count_values(str_word_count(strtolower($allContent), 1));
+        arsort($words);
+
+        $words = array_filter($words, function ($word) {
+            return strlen($word) > 4;
+        }, ARRAY_FILTER_USE_KEY);
+
+        return array_slice(array_keys($words), 0, 5);
+    }
+
+    /**
+     * @param Post[] $posts
+     * @return bool|int
+     */
+    private function updateMostUsedWords(array $posts)
+    {
+        $mostUsedWords = $this->countMostUsedWords($posts);
+
+        return file_put_contents($this->getFilePath('mostUsedWords.txt'), serialize($mostUsedWords));
+    }
+
+    /**
+     * @param string $fileName
+     * @return string
+     */
+    private function getFilePath(string $fileName): string
+    {
+        return $_SERVER['DOCUMENT_ROOT'] . "fileStorage/" . $fileName;
+    }
+
+    /**
+     * @return string
+     */
+    private function getImageDir(): string
+    {
+        return $_SERVER['DOCUMENT_ROOT'] . "public/uploaded/";
     }
 }
